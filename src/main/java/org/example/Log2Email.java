74ca224b17785b883f42d320a72d9a6b4759910c
nhandt4 <65377449+nhandt4@users.noreply.github.com>
2020-05-21 08:48:43 +0700

    Update Log2Email.java

--- src/main/java/org/example/Log2Email.java
+++ src/main/java/org/example/Log2Email.java
@@ -5,7 +5,7 @@ import org.apache.log4j.Logger;
 public class Log2Email implements Log{
     private static final Logger logger = Logger.getLogger(Log2Email.class);
     @Override
-    public void Write() {
-        logger.info("Email");
+    public void Write(String s) {
+        logger.info(s);
     }
 }
