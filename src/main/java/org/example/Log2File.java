5c9a5a629f287bb19e1172c88d96fd655585fdbc
nhandt4 <65377449+nhandt4@users.noreply.github.com>
2020-05-21 08:47:44 +0700

    Update Log2File.java

--- src/main/java/org/example/Log2File.java
+++ src/main/java/org/example/Log2File.java
@@ -6,7 +6,7 @@ import org.apache.log4j.Logger;
 public class Log2File implements Log {
     private static final Logger logger = Logger.getLogger(Log2File.class);
     @Override
-    public void Write() {
-        logger.info("File");
+    public void Write(String s) {
+        logger.info(s);
     }
 }
